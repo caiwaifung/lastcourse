0cd976b462ee770c10e4c163ae9de35d0c6f8bd1
Caiwai Fung <gdfqw93@163.com>
2015-05-26 12:43:56 +0800

    .

--- result/show.py
+++ result/show.py
@@ -29,19 +29,28 @@ f.close()
 f = open('a.html', 'w')
 print >>f, '<!DOCTYPE html>'
 print >>f, '<html> <body>'
-print >>f, '<table border="1">'
+print >>f, '<table>'
+print >>f, '<tr>'
+print >>f, '<td align="center">Query</td>'
+print >>f, '<td align="center"></td>'
+print >>f, '<td align="center">1st Closest</td>'
+print >>f, '<td align="center">2nd Closest</td>'
+print >>f, '<td align="center">3rd Closest</td>'
+print >>f, '<td align="center">4th Closest</td>'
+print >>f, '<td align="center">5th Closest</td>'
+print >>f, '</tr>'
 for i in range(min(100, len(ans))):
     std = b[i].split('_')[0]
-
     td = '<td align="center">'
-    template = 'alt="a0" style="height:80px;max-width:110px" align="center"'
+    template = 'alt="" style="height:50px;max-width:70px" align="center"'
     print >>f, '<tr>'
     print >>f, '{}<img src="../data/image/{}" {}></td>'.format(td, b[i], template)
-    print >>f, '<td width="50"></td>'
+    print >>f, '<td width="10"></td>'
     for k in ans[i]:
-        flag = 'Yes' if a[k].split('_')[0] == std else ''
-        print >>f, '{}<img src="../data/image/{}" {}></td>'.format(td, a[k], template)
-        print >>f, '{}{}</td>'.format(td, flag)
+        ok_icon = '<img src="ok-icon.png" alt="" style="height:15px;width:15px">' 
+        flag = ok_icon if a[k].split('_')[0] == std else ''
+        print >>f, '{}<img src="../data/image/{}" {}>'.format(td, a[k], template)
+        print >>f, '{}</td>'.format(flag)
     print >>f, '</tr>'
 print >>f, '</table>'
 print >>f, '</body> </html>'
