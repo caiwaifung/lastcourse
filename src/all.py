72b93bd97bf8bb55f05ec50441161bff5058425e
Caiwai Fung <gdfqw93@163.com>
2015-05-26 20:54:32 +0800

    .

--- src/all.py
+++ src/all.py
@@ -9,10 +9,11 @@ for k in [4, 8, 12, 16, 20, 24]:
 for k in [25]:
     a.append(('Composite{}'.format(k), k))
 
-for x in a:
-    print '{}:'.format(x[0])
-    cmd_r = 'python run.py a {} data5k {}'.format(x[0], x[1])
-    #cmd_g = 'grep "#cor\|#access\|#split"'
-    cmd_g = 'grep "#correct_first"'
-    os.system('{} | {}'.format(cmd_r, cmd_g))
+for d in [1, 2, 3, 4, 5]:
+    for x in a:
+        print '{}-{}:'.format(d,x[0])
+        cmd_r = 'python run.py a {} data{}k {}'.format(x[0], d,x[1])
+        #cmd_g = 'grep "#cor\|#access\|#split"'
+        cmd_g = 'grep "#correct_first"'
+        os.system('{} | {}'.format(cmd_r, cmd_g))
 
