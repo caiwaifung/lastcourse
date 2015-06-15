469a061d348249e1413d5fcd8173af437957980f
Pufan He <hpfdf@126.com>
2015-06-15 17:17:44 +0800

    change info

--- src2/main.m
+++ src2/main.m
@@ -23,42 +23,42 @@ result_file = '../result/res.txt';
 % Training
 if training
     % read the data & labels
+    fprintf('> getting labels...\n');
     labels_data = get_label(data_list);
-    fprintf('> label got.\n');
+    fprintf('> reading images and resizing...\n');
     img_data = read_and_resize(data_list, data_path, image_max_side, image_max_side);
-    fprintf('> resized.\n');
-    whos labels_data img_data;
     % extract patches
+    fprintf('> extracting patches...\n');
     patch_data = extract_patches(img_data); % num*(N-W+1)^2*P
-    fprintf('> extracted.\n');
-    whos patch_data;
+    %whos patch_data;
+    fprintf('> sampling...\n');
     patch_data_r = sample_patches(patch_data, kmeans_max_sample); % snum*P
-    fprintf('> sampled.\n');
-    whos patch_data_r;
+    %whos patch_data_r;
     % run kmeans
+    fprintf('> k-means clustering...\n');
     kms = kmeans_train(patch_data_r, kmeans_k); % matrix of K*P where P=W*W*3
     save(kmeans_model, 'kms');
-    fprintf('> kmeans finished.\n');
     whos kms;
     % extract features
+    
+    fprintf('> calculating k-means feature...\n');
     f1 = feature_kmeans(patch_data, kms);
-    fprintf('> kmeans featured.\n');
-    whos f1;
+    %whos f1;
+    
+    fprintf('> calculating color feature...\n');
     f2 = feature_cmhsv(img_data);
-    fprintf('> cm featured.\n');
-    whos f2;
+    %whos f2;
     f = [f1 f2];
     save(feature_model, 'f');
-    whos f;
-
+    
+    fprintf('> training svm...\n');
     svms = train_svm(f, labels_data);
     predict_svm(svms, f, labels_data);
     save(svm_model, 'svms');
-
-    fprintf('> svm trained.\n');
-    whos svms;
-
+    
+    fprintf('------------ done training -----------\n');
 else
+    fprintf('> loading models...\n');
     load(kmeans_model, 'kms');
     load(feature_model, 'f');
     load(svm_model, 'svms');
@@ -66,12 +66,24 @@ end
 
 % Main phase
 %  now we have: f (features), kms (kmeans model), svms (svm model)
+
+fprintf('> geting ground truth labels...\n');
 labels_test = get_label(test_list);
+
+fprintf('> reading and resizing test images...\n');
 img_test = read_and_resize(test_list, test_path, image_max_side, image_max_side);
+
+fprintf('> extracting patches for test images...\n');
 patch_test = extract_patches(img_test);
+
+fprintf('> calculating features for test images...\n');
 g1 = feature_kmeans(patch_test, kms);
 g2 = feature_cmhsv(img_test);
 g = [g1 g2];
+
+fprintf('> classifying by svm...\n');
 predict_label = predict_svm(svms, g, labels_test);
 
 dlmwrite(result_file, [predict_label g]);
+
+fprintf('------------ done evaluation -----------\n');
