69ebf5a40e9ace2489a152bf6853826394bf2d07
Caiwai Fung <gdfqw93@163.com>
2015-06-15 16:17:54 +0800

    .

--- src2/sample_patches.m
+++ src2/sample_patches.m
@@ -1,3 +1,11 @@
-function res = sample_patches(data, limit)
+function patches = sample_patches(data, limit)
+    num = size(data, 1);
+    patches = zeros(limit, 108);
+    for i = 1:limit
+        j = randi(num);
+        k = randi(size(data{j, 1}, 1));
+        assert(size(data{j, 1}, 2) == 108);
+        patches(i, :) = data{j, 1}(k, :);
+    end
 end
 
